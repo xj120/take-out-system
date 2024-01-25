b011afbed59a95d03e1c7cf855a17e1d04da12a7
ant12 <516622533@qq.com>
2024-01-25 10:14:49 +0800

    编辑员工

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.sky.controller.admin;
 
+import ch.qos.logback.core.recovery.ResilientFileOutputStream;
 import com.sky.constant.JwtClaimsConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
@@ -114,6 +115,31 @@ public class EmployeeController {
     public Result startOrStop(@PathVariable("status") Integer status, Long id){
         log.info("启用禁用人工账号：{},{}", status, id);
         employeeService.startOrStop(status, id);
+        return Result.success();
+    }
+
+    /**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询员工信息")
+    public Result<Employee> getById(@PathVariable Long id){
+        Employee employee = employeeService.getById(id);
+        return Result.success(employee);
+    }
+
+    /**
+     * 编辑员工信息
+     * @param employeeDTO
+     * @return
+     */
+    @PutMapping
+    @ApiOperation("编辑员工信息")
+    public Result update(@RequestBody EmployeeDTO employeeDTO){
+        log.info("编辑员工信息");
+        employeeService.update(employeeDTO);
         return Result.success();
     }
 }
