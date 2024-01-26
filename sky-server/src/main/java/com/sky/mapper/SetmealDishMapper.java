afe6b270e92236e21de30c13b4e4d0eba327abfa
ant12 <516622533@qq.com>
2024-01-26 19:47:04 +0800

    删除菜品

---
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -0,0 +1,15 @@
+package com.sky.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface SetmealDishMapper {
+    /**
+     * 给根据菜品ID查询对应的套餐ID
+     * @return
+     */
+    //select setmeal_id from setmeal_dish where dish_id in (1,2,3,4)
+    List<Long> getSetmealIdsByDishIds(List<Long> dishIds);
+}
