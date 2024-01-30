f8fdb56539c5f48102f52e553f8d4ddf1a93ccf3
ant12 <516622533@qq.com>
2024-01-30 20:31:13 +0800

    缓存套餐

--- sky-common/src/main/java/com/sky/exception/SetmealEnableFailedException.java
+++ sky-common/src/main/java/com/sky/exception/SetmealEnableFailedException.java
@@ -3,7 +3,8 @@ package com.sky.exception;
 /**
  * 套餐启用失败异常
  */
-public class SetmealEnableFailedException extends BaseException {
+public class
+SetmealEnableFailedException extends BaseException {
 
     public SetmealEnableFailedException(){}
 
