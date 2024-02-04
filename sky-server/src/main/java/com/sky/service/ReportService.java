ae622a7f15057f7be709cc73eacffb49abfc1b3a
ant12 <516622533@qq.com>
2024-02-04 21:00:41 +0800

    营业额统计

---
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -0,0 +1,16 @@
+package com.sky.service;
+
+import com.sky.vo.TurnoverReportVO;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+public interface ReportService {
+    /**
+     * 营业额统计
+     * @param begin
+     * @param end
+     * @return
+     */
+    TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end);
+}
